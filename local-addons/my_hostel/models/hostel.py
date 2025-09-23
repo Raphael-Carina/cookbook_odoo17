e8dfe25d8c330618066246ac6bade57d4567fa36
Raphael-Carina <rollier@carina.consulting>
2025-09-23 11:40:04 +0200

    Ajout nvx champs hostel.hostel + adaptation vue form

--- local-addons/my_hostel/models/hostel.py
+++ local-addons/my_hostel/models/hostel.py
@@ -12,12 +12,18 @@ class Hostel(models.Model):
     hostel_code = fields.Char(string="Code", required=True)
     street = fields.Char(string="Street")
     street2 = fields.Char(string="Street 2")
+    zip = fields.Char(string="Zip", change_default=True)
+    city = fields.Char(string="City")
+    phone = fields.Char(string="Phone", required=True)
+    mobile = fields.Char(string="Mobile", required=True)
+    email = fields.Char(string="Email")
 
     # ===================
     # Champs relationnels
     # ===================
 
     state_id = fields.Many2one(string="State", comodel_name="res.country.state")
+    country_id = fields.Many2one(string="Country", comodel_name="res.country")
 
     # =========
     # Overrides
