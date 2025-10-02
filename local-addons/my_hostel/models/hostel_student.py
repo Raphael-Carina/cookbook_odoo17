7123213b8ddd792627f7b237a59fa6cf9bf64eb0
Raphael-Carina <rollier@carina.consulting>
2025-10-02 11:48:32 +0200

    Ajout lien hostel.student --> hostel.room (Many2one)

--- local-addons/my_hostel/models/hostel_student.py
+++ local-addons/my_hostel/models/hostel_student.py
@@ -14,3 +14,23 @@ class HostelStudent(models.Model):
         selection=[('male','Male'),('female','Female'),('other','Other')],
         string="Gender"
     )
+
+    # ============================
+    # Champs relatifs à la chambre
+    # ============================
+
+    """
+    Un étudiant ne peut avoir qu'une chambre (et une chambre peut théoriquement avoir plusieurs étudiant) donc :
+
+    hostel.student --> hostel.room : Many2one
+    hostel.room --> hostel.student : One2many
+
+    Concrètement :
+    Sur un étudiant on peut voit la chambre qu'il occupe.
+    Sur un chambre on peut voir la liste de ses occupants.
+    """
+
+    room_id = fields.Many2one(
+        string="Room",
+        comodel_name='hostel.room'
+    )
